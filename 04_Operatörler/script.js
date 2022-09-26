1613b613fd873c326d50429c5d9db18d4e5b00aa
Ahmet Koçyiğit <63398372+Ahmetkcyigit@users.noreply.github.com>
2022-09-27 00:35:47 +0300

    Add files via upload

---
+++ 04_Operatörler/script.js
@@ -0,0 +1,29 @@
+let sonuc;
+let a = 10, b = 20, c = 30;
+
+// ARİTMETİK Operatörler
+sonuc = a + b;
+sonuc = a - b;
+sonuc = a * b;
+sonuc = a / b;
+sonuc = c % a;
+sonuc = a++; // a + 1
+sonuc = a--; // a - 1
+
+
+// ATAMA Operatörleri
+sonuc = a;
+sonuc += a // sonuc = sonuc + a   
+sonuc -= a // sonuc = sonuc - a   
+sonuc *= a // sonuc = sonuc * a   
+sonuc /= a // sonuc = sonuc / a   
+sonuc %= a // sonuc = sonuc % a   
+
+//KARŞILAŞTIRMA Operatörleri
+sonuc = (a == b) // a b ye eşit mi 
+sonuc = (a != b) // a b ye eşit değil mi 
+sonuc = (3 === "3") // değer ve tip karşılaştırması yapar
+sonuc = (a > b)
+sonuc = (a < b)
+
+console.log(sonuc);
