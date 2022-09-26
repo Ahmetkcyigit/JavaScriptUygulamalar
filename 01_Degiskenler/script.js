09bd203b365d35436648d947f798105cae39fc40
Ahmet Koçyiğit <63398372+Ahmetkcyigit@users.noreply.github.com>
2022-09-26 23:55:06 +0300

    Add files via upload

---
+++ 01_Degiskenler/script.js
@@ -0,0 +1,11 @@
+var maasAli = 7000;
+var maasCan = 6000;
+
+var zam = 0.3;
+
+console.log(maasAli + (maasAli * zam)) // maas ali
+console.log(maasCan + (maasCan * zam)) // maas can
+
+// Türkçe karakter kullanılmaz.
+// Arada boşluk olmaz.
+// Sayı ile başlamaz.
