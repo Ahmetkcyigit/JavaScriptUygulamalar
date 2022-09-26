df8bc8f256e1edacfa297c803728072438968f55
Ahmet Koçyiğit <63398372+Ahmetkcyigit@users.noreply.github.com>
2022-09-26 23:55:18 +0300

    Add files via upload

---
+++ 02_DegiskenTurleri/script.js
@@ -0,0 +1,32 @@
+var urunAdi = "iphone 13"; //string
+let urunFiyat = 15000; // number
+
+console.log(typeof urunAdi);
+console.log(typeof urunFiyat);
+
+// let sayi1 = 10;
+// let sayi2 = 20;
+// console.log(sayi1 + sayi2);
+
+// let sayi1 = "10";
+// let sayi2 = "20";
+// console.log(Number(sayi1) + Number(sayi2));
+
+let sayi1 = 10;
+let sayi2 = 20;
+console.log(sayi1.toString() + sayi2.toString());
+
+let isim = "ahmet";
+let soyad = "kocyigit";
+console.log(isim + " " + soyad);
+
+let sinavNotu = 40;
+let basarilimi = (sinavNotu >= 50);
+console.log(basarilimi);
+console.log(typeof basarilimi);
+
+let yas;
+console.log(yas);
+console.log(typeof yas);
+
+// ("") bu bir boşluk değeridir yani string.
