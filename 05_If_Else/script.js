c3f0355e6358635efd14bd25df93d77b0c08e86a
Ahmet Koçyiğit <63398372+Ahmetkcyigit@users.noreply.github.com>
2022-09-28 00:46:54 +0300

    Add files via upload

---
+++ 05_If_Else/script.js
@@ -0,0 +1,17 @@
+let username = "ahmetkocyıgıt";
+let password = "1234";
+
+if (username == "ahmetkocyıgıt") {
+    if (password == "1234") {
+        console.log("giriş yapıldı");
+    } else {
+        console.log("parola yanlış.")
+    }
+}
+else {
+    console.log("kullanıcı adı yanlış");
+}
+
+// if (!isLoggedin) {
+//     console.log("merhaba");
+// }
